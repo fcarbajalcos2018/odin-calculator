b920e9705a3d772cc1d0163032eb4b58bef18b7c
fcarbajalcos2018 <fcarbajalcos2018@fau.edu>
2024-02-23 12:14:30 -0500

    Reimplemented raw code for calculate function.

--- calculate.js
+++ calculate.js
@@ -2,33 +2,42 @@ const userInput = ['12', '-', '2', '*', '2'];
 console.log(userInput);
 
 function calculate(userInput){
-    let increment = 0;
-    for (let i = 0; i < userInput.length; i++){
-        const currentUI = parseInt(userInput[i]);
-        if (i == 0){
-            increment = currentUI;
-            continue;
+    let new_userInput = userInput.slice(0);
+    const operators = ['*', '/', '+', '-'];
+    for (const operator of operators){
+        let operationIndex = [];
+        for (const os in new_userInput){
+            if (operator === new_userInput[os]){
+                operationIndex.push(os);
+            }
         }
-        if (i % 2 != 0){
-            continue;
-        }
-        switch (userInput[i - 1]){
+        const operationStart = operationIndex[0];
+        const operationFinish = operationIndex[operationIndex.length - 1];
+        const splitList1 = new_userInput.slice(0, operationStart - 1);
+        const splitList2 = new_userInput.slice(operationStart - 1, operationFinish + 2);
+        let solution;
+        const operands = splitList2.filter(elem => parseInt(elem))
+                                   .map(elem => parseInt(elem));
+        switch (operator){
             case '+':
-                increment += currentUI;
+                solution = operands.reduce((p, c) => p + c);
                 break;
             case '-':
-                increment -= currentUI;
+                solution = operands.reduce((p, c) => p - c);
                 break;
             case '*':
-                increment *= currentUI;
+                solution = operands.reduce((p, c) => p * c);
                 break;
             case '/':
-                increment /= currentUI;
+                if (!operands.includes(0)){
+                    solution = operands.reduce((p, c) => p / c);
+                }
                 break;
             default:
                 break;
         }
+        splitList1.push(solution)
+        new_userInput = splitList1.slice(0);
     }
-    return increment;
 }
 console.log(calculate(userInput));
